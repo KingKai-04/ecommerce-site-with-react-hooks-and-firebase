e6da6b7c93779348c1b2c4a98c40ced03137879a
KingKai-04 <Laughingtoad417@gmail.com>
2022-10-09 13:17:37 -0400

    E-commerce With Multiple Props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './Components/Home'
+import { Login } from './Components/Login'
+import { Signup } from './Components/Signup'
+import { NotFound } from './Components/NotFound'
+import { AddProducts } from './Components/AddProducts'
+import { Cart } from './Components/Cart'
 
 export const App = () => {
   return (
-    <div>
-      ecommerce site with react hooks and firebase
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" component = {<Home/>}/>
+        <Route path="/signup" component={Signup}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/add-products" component={AddProducts}/>
+        <Route path="/cart" component={Cart}/>       
+        <Route component={NotFound}/>        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
